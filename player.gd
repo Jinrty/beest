3e7a9f82d933c94bb077bfd4df51eec9d13258c8
Jinrty <huberthubertza@gmail.com>
2026-06-09 22:25:23 +0200

    Player movement
    apart from basics like jump I added:
    -coyote time
    -jump buffering
    - pause beetwen jumps

--- player.gd
+++ player.gd
@@ -1,25 +1,73 @@
 extends CharacterBody2D
 
+const SPEED:float = 350.0
+const JUMP_VELOCITY:float = -500.0
+var can_jump:bool = true
+var proccesing_coyote:bool = false
+var already_on_floor:bool = false
+var buffer_jump:bool = false
+var gravity:int = 1700
+var time_in_air:float = 0.0
+var direction:int
+var jump_pause:bool = false
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+func take_input() -> void:
+	direction = Input.get_axis("move_left", "move_right")
+
+	if Input.is_action_pressed("jump"):
+		if can_jump == true:
+			jump()
+		elif not is_on_floor():
+			$Jump_buffering.start()
+			buffer_jump = true
+	
+
+func jump() -> void:
+	if(jump_pause == false):
+		buffer_jump = false
+		velocity.y = JUMP_VELOCITY
+		can_jump = false
+		already_on_floor = false
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	take_input()
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+
+	if not is_on_floor():
+
+		time_in_air += delta / 2.5
+		velocity.y += gravity * delta * (1.0 + time_in_air * 0.5)
+
+		if proccesing_coyote == false:
+			$Coyot.start()
+			proccesing_coyote = true
+			already_on_floor = false
+		
+	if is_on_floor():
+		if(jump_pause == false):
+			jump_pause = true
+			$Pause_in_jumps.start()
+		can_jump = true
+		time_in_air = 0
+		proccesing_coyote = false
+		already_on_floor = true
+		if buffer_jump == true:
+			jump()
+			$Jump_buffering.stop()
+	
 	move_and_slide()
+
+func _on_coyot_timeout() -> void:
+	can_jump = false
+
+func _on_jump_buffering_timeout() -> void:
+	buffer_jump = false
+
+func _on_pause_in_jumps_timeout() -> void:
+	jump_pause = false
