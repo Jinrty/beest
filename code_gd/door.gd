1d5cbabbd9cdfd8e4616a5d4dcf6fd3a2102d9b7
Jinrty <huberthubertza@gmail.com>
2026-06-25 19:05:42 +0200

    Next part of corridor and door requirment
    -  next part of corridor with all sprites
    - doors locked
    - doors blocked by npcs

--- code_gd/door.gd
+++ code_gd/door.gd
@@ -2,9 +2,16 @@
 extends Area2D
 
 @export var where:Area2D
+@export var requires_item_use:bool = false
+
+@export var requires_item_npc:bool = false
+@export var npc_gate:Node2D = null
+@export var refuse_line:String = ""
 
 var inside:bool = false
 var player:Node2D = null
+
+const DOOR = preload("res://sprites/corridor2/door.png")
 
 @export var sprite:Texture2D:
 	set(value):
@@ -14,7 +21,26 @@ var player:Node2D = null
 	
 func _process(delta: float) -> void:
 	if(inside == true and Input.is_action_just_pressed("interact")):
-		player.teleport(where)
+		if(player == null):
+			return
+		if(requires_item_use == false) and (requires_item_npc == false):
+			player.teleport(where)
+			return
+		
+		if(requires_item_use == true):
+			if(player.item_name() == "Crowbar"):
+				$Sprite2D.texture = DOOR
+				player.say("Yeah!")
+				requires_item_use = false
+			else:
+				player.say("I can't go here!")
+				
+		elif(requires_item_npc == true):
+			if(player.item_name() == "Gentleman hat"):
+				player.teleport(where)
+			else:
+				npc_gate.get_node("DialogueComponent").say(refuse_line)
+			
 		
 func _ready() -> void:
 	get_parent().set_editable_instance(self, true)
