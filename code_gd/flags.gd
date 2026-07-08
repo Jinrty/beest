565dc44ac037af570cfdb7f4b8fc2a75c1f89734
Jinrty <huberthubertza@gmail.com>
2026-07-08 16:10:02 +0200

    Quests v2
    - couple quests
    - I dont have energy to write all of them! Just play the game!

--- code_gd/flags.gd
+++ code_gd/flags.gd
@@ -3,6 +3,7 @@ extends Node
 
 var pipe_block:bool = false
 
+var green_kid_explained:bool = false
 var green_kid_quest_completed:bool = false
 
 var first_encounter_with_heart_left:bool = true
@@ -10,3 +11,19 @@ var heart_left_quest_completed:bool = false
 
 var first_encounter_with_heart_right:bool = true
 var heart_right_quest_completed:bool = false
+
+var first_encounter_with_yellow:bool = true
+var prank_on_yellow:bool = false
+
+var start_white_x_quest:bool = false
+var quest_with_white_x_completed:bool = false
+
+var started_post_quest:bool = false
+var love_given:bool = false
+var got_post_number:bool = false
+var purple_guard_quest_completed:bool = false
+
+var apologize_quest_start:bool = false
+var red_bean_command:bool = false
+var apologized:bool = false
+var apologize_completed:bool = false
