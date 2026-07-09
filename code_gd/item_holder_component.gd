38148cc054e705774a36ced6d4fcab2305a06b6d
Jinrty <huberthubertza@gmail.com>
2026-07-09 23:02:16 +0200

    item holder bug fix
    - previously the game crashed when some specific actions happened

--- code_gd/item_holder_component.gd
+++ code_gd/item_holder_component.gd
@@ -2,12 +2,17 @@ extends Node2D
 
 @export var item:Node2D
 var player:Node2D
+var used:bool = false
 
 func on_interact() -> void:
 	player = get_parent().player
-	if(player.has_item(item.title) == false):
+	
+	if(used):
+		player.say(get_parent().already_has_line)
+	elif(player.has_item(item.title) == false):
 		player.say(get_parent().answer)
 		await get_tree().create_timer(2).timeout
 		player.add_item(item)
+		used = true
 	else:
 		player.say(get_parent().already_has_line)
